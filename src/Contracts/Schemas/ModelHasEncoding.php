ba113f62b731dfdd8d56e160f19405661c18b742
Hamzah <hamzahnafalah@gmail.com>
2025-07-13 12:34:53 +0700

    update modules

--- src/Contracts/Schemas/ModelHasEncoding.php
+++ src/Contracts/Schemas/ModelHasEncoding.php
@@ -7,13 +7,22 @@ use Hanafalah\ModuleEncoding\Contracts\Data\ModelHasEncodingData;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @see \Hanafalah\ModuleEncoding\Schemas\ModelHasEncoding
+ * @method self setParamLogic(string $logic, bool $search_value = false, ?array $optionals = [])
+ * @method self conditionals(mixed $conditionals)
+ * @method bool deleteModelHasEncoding()
+ * @method mixed getModelHasEncoding()
+ * @method ?Model prepareShowModelHasEncoding(?Model $model = null, ?array $attributes = null)
+ * @method array showModelHasEncoding(?Model $model = null)
+ * @method Collection prepareViewModelHasEncodingList()
+ * @method array viewModelHasEncodingList()
+ * @method LengthAwarePaginator prepareViewModelHasEncodingPaginate(PaginateData $paginate_dto)
+ * @method array viewModelHasEncodingPaginate(?PaginateData $paginate_dto = null)
+ * @method array storeModelHasEncoding(?ModelHasEncodingData $model_has_encoding_dto = null)
+ * @method Builder modelHasEncoding(mixed $conditionals = null)
+ */
 interface ModelHasEncoding extends DataManagement
 {
-    public function getModelHasEncoding(): mixed;
-    public function prepareShowModelHasEncoding(?Model $model = null, ?array $attributes = null): Model;
-    public function showModelHasEncoding(?Model $model = null): array;
     public function prepareStoreModelHasEncoding(ModelHasEncodingData $model_has_encoding_dto): Model;
-    public function storeModelHasEncoding(? ModelHasEncodingData $model_has_encoding_dto = null): array;
-    public function hasEncoding(mixed $conditionals = null): Builder;
-    
 }
--- src/Schemas/ModelHasEncoding.php
+++ src/Schemas/ModelHasEncoding.php
@@ -14,55 +14,17 @@ class ModelHasEncoding extends PackageManagement implements ContractsModelHasEnc
     protected string $__entity = 'ModelHasEncoding';
     public static $model_has_encoding_model;
 
-    public function getModelHasEncoding(): mixed{
-        return static::$model_has_encoding_model;
-    }
-
-    public function prepareShowModelHasEncoding(?Model $model = null, ?array $attributes = null): Model{
-        $attributes ??= request()->all();
-
-        $model ??= $this->getModelHasEncoding();
-        if (!isset($model)) {
-            $id   = $attributes['id'] ?? null;
-            if (!isset($id)) throw new \Exception('id is not found');
-
-            $model = $this->hasEncoding()->with($this->showUsingRelation())->findOrFail($id);            
-        } else {
-            $model->load($this->showUsingRelation());
-        }
-        return static::$model_has_encoding_model = $model;
-    }    
-
-    public function showModelHasEncoding(?Model $model = null): array{
-        return $this->showEntityResource(function() use ($model){
-            return $this->prepareShowModelHasEncoding($model);
-        });
-    }
-
     public function prepareStoreModelHasEncoding(ModelHasEncodingData $model_has_encoding_dto): Model{
-        $model = $this->ModelHasEncodingModel()->updateOrCreate([
+        $model = $this->usingEntity()->updateOrCreate([
             'encoding_id'    => $model_has_encoding_dto->encoding_id,
             'reference_id'   => $model_has_encoding_dto->reference_id,
             'reference_type' => $model_has_encoding_dto->reference_type
+        ],[
+            'value' => $model_has_encoding_dto->value
         ]);
-        foreach ($model_has_encoding_dto->props as $key => $prop) {
-            $model->{$key} = $prop;
-        }
+        $this->fillingProps($model,$model_has_encoding_dto->props);
         $model->save();
         return static::$model_has_encoding_model = $model;
-    }
-
-    public function storeModelHasEncoding(? ModelHasEncodingData $model_has_encoding_dto = null): array{
-        return $this->transaction(function () use ($model_has_encoding_dto) {
-            return $this->showModelHasEncoding($this->prepareStoreModelHasEncoding($model_has_encoding_dto ?? $this->requestDTO(ModelHasEncodingData::class)));
-        });
-    }
-
-    public function hasEncoding(mixed $conditionals = null): Builder{
-        $this->booting();
-        return $this->ModelHasEncodingModel()
-                    ->conditionals($this->mergeCondition($conditionals))
-                    ->withParameters();
     }
 }
 
